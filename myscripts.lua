b3cde9d435d3ebe02379ecc9c6938b07c15e7c27
crceck123 <100026828+crceck123@users.noreply.github.com>
2024-04-29 00:10:24 +0400

    Update myscripts.lua

--- myscripts.lua
+++ myscripts.lua
@@ -1,7 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/Chillz-s-scripts/main/Synapse-X-Remake.lua"))()
 -- loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))() --
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RS/main/SimpleSpyMobile"))()
 loadstring(game:HttpGet("https://scriptblox.com/raw/Prison-Life-tiger-admin-Feb-1-working-10302"))()
 wait(1)
