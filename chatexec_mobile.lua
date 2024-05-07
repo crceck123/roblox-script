f280fbfd15b00f0652711a118ed2224471218436
crceck123 <100026828+crceck123@users.noreply.github.com>
2024-05-07 22:46:04 +0400

    Update chatexec_mobile.lua

--- chatexec_mobile.lua
+++ chatexec_mobile.lua
@@ -1,5 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zephyr10101/r5gf6s8hjs9/main/chat-executor",true))()
-wait(5)
+wait(15)
 game.Players.LocalPlayer.PlayerGui.ScriptGUI.Frame.Active = true
 game.Players.LocalPlayer.PlayerGui.ScriptGUI.Frame.Draggable = true
 wait(120)
