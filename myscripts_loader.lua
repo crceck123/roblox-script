640cf3a154cf5f51faac9d0bb0c658a233a2be88
crceck123 <100026828+crceck123@users.noreply.github.com>
2024-05-07 22:45:25 +0400

    Update myscripts_loader.lua

--- myscripts_loader.lua
+++ myscripts_loader.lua
@@ -1,29 +1,9 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 loadstring(game:HttpGet("https://pastebin.com/raw/WxS2CFBX"))()
-if game.Players.LocalPlayer.PlayerGui.ScreenGui.Toogle.Corner do
-game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
-  else
-    game.Players.LocalPlayer.PlayerGui.ScreenGui.Name = "ScreenGui1ingame"
-    if game.Players.LocalPlayer.PlayerGui.ScreenGui.Toogle.Corner do
-game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
-  else
-    game.Players.LocalPlayer.PlayerGui.ScreenGui.Name = "ScreenGui1ingame"
-        if game.Players.LocalPlayer.PlayerGui.ScreenGui.Toogle.Corner do
-game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
-  else
-    game.Players.LocalPlayer.PlayerGui.ScreenGui.Name = "ScreenGui1ingame"
-            if game.Players.LocalPlayer.PlayerGui.ScreenGui.Toogle.Corner do
-game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
-  else
-    game.Players.LocalPlayer.PlayerGui.ScreenGui.Name = "ScreenGui1ingame"
-game.Players.LocalPlayer.PlayerGui.ScreenGui1ingame.Name = "ScreenGui"
-game.Players.LocalPlayer.PlayerGui.ScreenGui1ingame.Name = "ScreenGui"
-game.Players.LocalPlayer.PlayerGui.ScreenGui1ingame.Name = "ScreenGui"
-game.Players.LocalPlayer.PlayerGui.ScreenGui1ingame.Name = "ScreenGui"
-game.Players.LocalPlayer.PlayerGui.ScreenGui1ingame.Name = "ScreenGui"
-game.Players.LocalPlayer.PlayerGui.ScreenGui1ingame.Name = "ScreenGui"
-game.Players.LocalPlayer.PlayerGui.ScreenGui1ingame.Name = "ScreenGui"
-
+if game.Players.LocalPlayer.PlayerGui.ScreenGui.Toogle.Corner
+  game.Players.LocalPlayer.PlayerGui.ScreenGui:Destroy()
+else
+  print("can't delete screengui")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FD2Team/Nameless-Admin-No-Byfron-Kick/main/Source",true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/myscripts.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/chatexec_mobile.lua"))()
