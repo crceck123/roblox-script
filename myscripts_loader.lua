c46a402f8fe91624ee0b31813acefe63a0a441ff
crceck123 <100026828+crceck123@users.noreply.github.com>
2024-05-07 22:56:56 +0400

    Update myscripts_loader.lua

--- myscripts_loader.lua
+++ myscripts_loader.lua
@@ -1,6 +1,7 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 loadstring(game:HttpGet("https://pastebin.com/raw/WxS2CFBX"))()
-game.Players.LocalPlayer.PlayerGui.ScreenGui.Toogle.Visible = false
+wait(3)
+game.Players.LocalPlayer.PlayerGui.ScreenGui.Toogle:Destroy()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FD2Team/Nameless-Admin-No-Byfron-Kick/main/Source",true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/myscripts.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/chatexec_mobile.lua"))()
