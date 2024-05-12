577b045112bcd7f554b8282b70c87638dee33b69
crceck123 <100026828+crceck123@users.noreply.github.com>
2024-05-12 13:08:17 +0400

    Update myscripts.lua

--- myscripts.lua
+++ myscripts.lua
@@ -2,6 +2,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/Chillz-s-s
 -- loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Dex%20Explorer.txt"))() --
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RS/main/SimpleSpyMobile"))()
-loadstring(game:HttpGet("https://scriptblox.com/raw/Prison-Life-tiger-admin-Feb-1-working-10302"))()
 loadstring(game:HttpGet('https://astroswrld.club/BetterRoblox/Loader'))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/chatexec_mobile.lua"))()
+
+loadstring(game:HttpGet("https://scriptblox.com/raw/Prison-Life-tiger-admin-Feb-1-working-10302"))()
