2e5042e1a4eed487e5e26db55b48a982b5a7de6e
crceck123 <100026828+crceck123@users.noreply.github.com>
2024-05-19 14:58:06 +0400

    Update hydrogen_skin_for_evon.lua

--- hydrogen_skin_for_evon.lua
+++ hydrogen_skin_for_evon.lua
@@ -1,5 +1,5 @@
 game:GetService("CoreGui").androidCheat.toggleMain.icon.Image = "rbxassetid://17533786790"
-game:GetService("CoreGui").androidCheat.toggleMain.icon.ImageColor3 = Color3.fromRGB(255, 255, 255)
+game:GetService("CoreGui").androidCheat.toggleMain.icon.ImageColor3 = Color3.new(1,1,1)
 game:GetService("CoreGui").androidCheat.main.left.icon.Image = "rbxassetid://17533786790"
-game:GetService("CoreGui").androidCheat.main.left.icon.ImageColor3 = Color3.fromRGB(255, 255, 255)
+game:GetService("CoreGui").androidCheat.main.left.icon.ImageColor3 = Color3.new(1,1,1)
 print("Only on evon")
