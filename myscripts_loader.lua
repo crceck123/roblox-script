5bbed2c09d0ff89fb31a59db230cfd2afb6dbd1f
crceck123 <100026828+crceck123@users.noreply.github.com>
2024-08-14 16:12:25 +0400

    Update myscripts_loader.lua

--- myscripts_loader.lua
+++ myscripts_loader.lua
@@ -1,5 +1,5 @@
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/hydrogen_skin_for_evon.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 loadstring(game:HttpGet("https://pastebin.com/raw/WxS2CFBX"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FD2Team/Nameless-Admin-No-Byfron-Kick/main/Source",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))();
 loadstring(game:HttpGet("https://raw.githubusercontent.com/crceck123/roblox-script/main/myscripts.lua"))()
